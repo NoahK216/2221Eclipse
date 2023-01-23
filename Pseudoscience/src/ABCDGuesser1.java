3c614804f916c124de690ac454001bdb86b4b877
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2023-01-23 15:09:54 -0500

    added new project

---
+++ Pseudoscience/src/ABCDGuesser1.java
@@ -0,0 +1,57 @@
+import components.simplereader.SimpleReader;
+import components.simplereader.SimpleReader1L;
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Monte Carlo Estimate: compute percentage of pseudo-random points inside a
+ * unit circle on a 2 by 2 plane.
+ */
+public final class ABCDGuesser1 {
+
+    /**
+     * Repeatedly asks the user for a positive real number until the user enters
+     * one. Returns the positive real number.
+     *
+     * @param in
+     *            the input stream
+     * @param out
+     *            the output stream
+     * @return a positive real number entered by the user
+     */
+    private static double getPositiveDouble(SimpleReader in, SimpleWriter out) {
+        return 0;
+    }
+
+    /**
+     * Repeatedly asks the user for a positive real number not equal to 1.0
+     * until the user enters one. Returns the positive real number.
+     *
+     * @param in
+     *            the input stream
+     * @param out
+     *            the output stream
+     * @return a positive real number not equal to 1.0 entered by the user
+     */
+    private static double getPositiveDoubleNotOne(SimpleReader in,
+            SimpleWriter out) {
+        return 0;
+    }
+
+    /**
+     * Main method.
+     *
+     * @param args
+     *            the command line arguments; unused here
+     */
+    public static void main(String[] args) {
+        SimpleReader input = new SimpleReader1L();
+        SimpleWriter output = new SimpleWriter1L();
+
+        /*
+         * Close input and output streams
+         */
+        input.close();
+        output.close();
+    }
+}
