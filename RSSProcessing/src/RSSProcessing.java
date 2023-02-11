0c173cdd4d60cea56d2d4763ce5b5a76d4518ca2
NoahK216 <115042520+NoahK216@users.noreply.github.com>
2023-02-11 12:39:55 -0500

    Continued Project 4

--- RSSProcessing/src/RSSProcessing.java
+++ RSSProcessing/src/RSSProcessing.java
@@ -25,6 +25,33 @@ public final class RSSProcessing {
     }
 
     /**
+     * Creates file at specified directory with given String of contents.
+     *
+     * @param fileName
+     *            path for file to be created including extension
+     * @param contents
+     *            String to be written into fileName
+     * @param append
+     *            false if you would like to overwrite any existing text in the
+     *            file, true to append
+     * @requires [fileName must not have any characters invalid for fileName or
+     *           spaces]
+     * @ensures file with name fileName will be created at the base directory of
+     *          this project filled with contents
+     */
+    private static void writeToFile(String fileName, String contents,
+            boolean append) {
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter(fileName, append))) {
+            writer.append(contents);
+            writer.close();
+
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    /**
      * Finds the first occurrence of the given tag among the children of the
      * given {@code XMLTree} and return its index; returns -1 if not found.
      *
@@ -81,10 +108,53 @@ public final class RSSProcessing {
                 + "Violation of: the label root of item is an <item> tag";
         assert out.isOpen() : "Violation of: out.is_open";
 
+        /*
+         * If title is present in the item populate the "News" column with it,
+         * otherwise populate it with the item's description
+         */
+        String news = "No title or description avaliable";
         if (getChildElement(item, "title") != -1) {
+            XMLTree titleTree = item.child(getChildElement(item, "title"));
+            if (titleTree.numberOfChildren() == 1) {
+                news = titleTree.child(0).label();
+            }
+        } else {
+            XMLTree descriptionTree = item
+                    .child(getChildElement(item, "description"));
+            if (descriptionTree.numberOfChildren() == 1) {
+                news = descriptionTree.child(0).label();
+            }
+        }
+
+        String newsLink = "";
+        if (getChildElement(item, "link") != -1) {
+            newsLink = item.child(getChildElement(item, "link")).child(0)
+                    .label();
+        }
+
+        String source = "No source available";
+        String sourceLink = "";
+        if (getChildElement(item, "source") != -1) {
+            XMLTree sourceTree = item.child(getChildElement(item, "source"));
+            sourceLink = sourceTree.attributeValue("url");
+            if (sourceTree.numberOfChildren() == 1) {
+                source = sourceTree.child(0).label();
+            }
 
         }
 
+        String pubDate = "";
+        if (getChildElement(item, "pubDate") != -1) {
+            pubDate = item.child(getChildElement(item, "pubDate")).child(0)
+                    .label();
+        }
+
+        String htmlRow = ("  <tr>\n" + "   <td>" + pubDate + "</td>\n"
+                + "   <td><a href=\"" + sourceLink + "\">" + source
+                + "</a></td>\n" + "   <td><a href=\"" + newsLink + "\">" + news
+                + "</a></td>\n" + "  </tr>\n");
+
+        writeToFile("output.html", htmlRow, true);
     }
 
     /**
@@ -96,31 +166,14 @@ public final class RSSProcessing {
      *
      * @return [line + "\n"]
      */
-    private static String newLine(String line) {
-        return (line + "\n");
-    }
-
-    /**
-     * Creates file at specified directory with given String of contents.
-     *
-     * @param fileName
-     *            path for file to be created including extension
-     * @param contents
-     *            String to be written into fileName
-     * @requires [fileName must not have any characters invalid for fileName or
-     *           spaces]
-     * @ensures file with name fileName will be created at the base directory of
-     *          this project filled with contents
-     */
-    private static void writeToFile(String fileName, String contents) {
-        try (BufferedWriter writer = new BufferedWriter(
-                new FileWriter(fileName))) {
-            writer.write(contents);
-            writer.close();
-
-        } catch (IOException error) {
-            error.printStackTrace();
-        }
+    private static String htmlHeader(String title, String titleLink,
+            String description) {
+        return ("<html>\n" + "<head>\n" + "<title>" + title + "</title>\n"
+                + "</head>\n" + "<body>\n" + " <h1><a href=\"" + titleLink
+                + "\">" + title + "</a></h1>\n" + " <p>" + description
+                + "</p>\n" + "<table border=\"1\">" + "\n" + "  <tr>\n"
+                + "   <th>Date</th>\n" + "   <th>Source</th>\n"
+                + "   <th>News</th>\n" + "  </tr>\n");
     }
 
     /**
@@ -138,31 +191,63 @@ public final class RSSProcessing {
         /*
          * Input the source URL.
          */
-        //out.print("Enter the URL of an RSS 2.0 news feed: ");
-        //String url = in.nextLine();
+        out.print("Enter the URL of an RSS 2.0 news feed: ");
+        String url = in.nextLine();
         /*
          * Read XML input and initialize XMLTree. If input is not legal XML,
          * this statement will fail.
          */
-        XMLTree xml = new XMLTree1(
-                "https://www.feedforall.com/sample-feed.xml");
+
+        XMLTree xml = new XMLTree1(url);
         /*
          * Extract <channel> element.
          */
         XMLTree channel = xml.child(0);
 
-        String output = newLine("Very funny");
-        output += newLine("balling");
+        String link = channel.child(getChildElement(channel, "link")).child(0)
+                .label();
 
-        /*-
-         * TODO: #4 - for each item, output title (or description, if title is
-         * not available) and link (if available)
-         */
+        String title = "No title avaliable";
+        if (channel.child(getChildElement(channel, "title"))
+                .numberOfChildren() == 1) {
+            title = channel.child(getChildElement(channel, "title")).child(0)
+                    .label();
+        }
+
+        String description = "No description avaliable";
+        if (channel.child(getChildElement(channel, "description"))
+                .numberOfChildren() == 1) {
+            description = channel.child(getChildElement(channel, "description"))
+                    .child(0).label();
+        }
 
         /*
-         * Write final output to output.html
+         * Begin html file with necessary tags and first table header, overwrite
+         * any existing files.
          */
-        writeToFile("output.html", output);
+        writeToFile("output.html", htmlHeader(title, link, description), false);
+
+        /*
+         * Ensure that the RSS feed has an item before processing them
+         */
+        if (getChildElement(channel, "item") != -1) {
+            /*
+             * Search through all children of channel as there is no guaranteed
+             * order
+             */
+            for (int i = 0; i < channel.numberOfChildren(); i++) {
+                if (channel.child(i).label().equals("item")) {
+                    processItem(channel.child(i), out);
+                }
+            }
+
+        }
+
+        /*
+         * Close initial HTML tags
+         */
+        writeToFile("output.html", "</table>\n" + "</body>\n" + "</html>",
+                true);
 
         /*
          * Close I/O streams.
